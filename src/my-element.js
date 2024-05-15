d56a8b1e8d1ae469f9ed0839685e723f8d2eb52b
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-05-15 17:44:37 -0500

    feat: :construction: Query of the album for the card in process

--- src/my-element.js
+++ src/my-element.js
@@ -77,12 +77,35 @@ class Cardnewmusic extends LitElement {
           <img src="./src/assets/humble.jpeg" alt="">
         </div>
         <div class="new__music__info">
-          <h4>Humble</h4>
+          <h4 @click="${this.getInfoCard}">Humble</h4>
           <p>Kendrick Lamar, <span>2017</span></p>
         </div>
       </div>
     </div>
     `
+  }
+
+
+  
+  async getInfoCard(){
+    let data = ["18NOKLkZETa4sWwLMIm0UZ", "444LqH6QlvR62nY8Vxn37u", "5r36AJ6VOJtp00oxSkBZ5h", "4jox3ip1I39DFC2B7R5qLH"]
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '81878cbf8bmsh6237fac9a4dbdcbp1b2a43jsnad3e05700a4d',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+      }
+    }
+    for (let val of data){
+      let res = await fetch(`https://spotify23.p.rapidapi.com/albums/?ids=${val}`, options)
+      let album = await res.json()
+      console.log(album)
+      //const name = {albums:[{name}]}
+      let {albums:[{name}]} = album
+      album = name
+      // album = albums
+      // console.log(nombredelacacion)
+    }
   }
 }
 
